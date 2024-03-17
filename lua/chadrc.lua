563742a4ad7aa1de59499e0dfa6808ea100e5422
Hoang Anh <hoanganhleboy@gmail.com>
2024-03-18 02:27:15 +0700

    custom starter page

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -13,6 +13,15 @@ M.ui = {
   },
   nvdash = {
     load_on_startup = true,
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰙅  Open Nvim Tree", "Spc c", "NvimTreeOpen"},
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  View Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Change Themes", "Spc t h", "Telescope themes" },
+      { "  View Key Maps", "Spc c h", "NvCheatsheet" },
+    },
   },
 }
 

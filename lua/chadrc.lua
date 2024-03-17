3ca90356ae9d0d225ae8d2c4bb9a53ca10f762ac
Hoang Anh <hoanganhleboy@gmail.com>
2024-03-18 01:47:27 +0700

    add config for statusline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,6 +6,11 @@ M.ui = {
     --  more opts
     order = { "treeOffset", "buffers", "tabs", "btns" },
   },
+  statusline = {
+    theme = "minimal",
+    separator_style = "round",
+    overriden_module = nil,
+  },
 }
 
 return M

14778333cdede54ed0a2bfd8b10eda88e635bb2b
Hoang Anh <hoanganhleboy@gmail.com>
2024-03-18 01:36:30 +0700

    some changes in config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,6 +2,10 @@ local M = {}
 
 M.ui = {
   theme = "chadracula",
+  tabufline = {
+    --  more opts
+    order = { "treeOffset", "buffers", "tabs", "btns" },
+  },
 }
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,5 +11,27 @@ return {
     opts = {
       git = { enable = true },
     },
-  },  
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+        "vimdoc",
+
+        -- web dev 
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "jsx",
+
+       -- low level
+        "go"
+      },
+    },
+  }
 }

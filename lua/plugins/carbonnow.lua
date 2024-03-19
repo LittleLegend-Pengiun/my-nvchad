a753ab3bcd687b059c37a715a8adba0c64874d5a
Hoang Anh <hoanganhleboy@gmail.com>
2024-03-19 19:54:57 +0700

    Add carbon-now to screenshot

---
+++ lua/plugins/carbonnow.lua
@@ -0,0 +1,22 @@
+local plugin = {
+	{
+		"ellisonleao/carbon-now.nvim",
+		lazy = true,
+		-- event = "VeryLazy",
+		cmd = "CarbonNow",
+		---@param opts cn.ConfigSchema
+		config = function()
+			local carbon = require("carbon-now")
+			carbon.setup({
+				options = {
+					theme = "material",
+					font_family = "Hack",
+				},
+				open_cmd = "open-cli",
+			})
+		end,
+		opts = {},
+	},
+}
+
+return plugin

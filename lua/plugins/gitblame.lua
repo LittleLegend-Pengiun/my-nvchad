1fb826dd43e0ce7ac458c2a00b319aac41da450b
Hoang Anh <hoanganhleboy@gmail.com>
2024-03-23 22:19:31 +0700

    Config for gitblame

--- lua/plugins/gitblame.lua
+++ lua/plugins/gitblame.lua
@@ -2,6 +2,15 @@ local plugin = {
 	{
 		"f-person/git-blame.nvim",
 		event = "VeryLazy",
+		config = function()
+			require("gitblame").setup({
+				enabled = true,
+				message_template = "<committer> • <date> • <summary>",
+				date_format = "%r",
+				message_when_not_committed = "You • Not committed!",
+				schedule_event = "CursorMoved",
+			})
+		end,
 	},
 }
 

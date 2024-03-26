e20a60d633f704c50cef90570c1468013dcbbdc0
Hoang Anh <hoanganhleboy@gmail.com>
2024-03-26 08:04:32 +0700

    Refactor the config of statusline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,16 +1,6 @@
 local M = {}
 -- local hightlights = require("configs.hightlights")
 
--- Use this for default style
--- local custom_sep_style = { left = "█", right = "" }
-
--- Use this for minimal style
-local sep_l = ""
-local sep_r = "%#St_sep_r#" .. "" .. " %#ST_EmptySpace#"
-local function gen_block(icon, txt, sep_l_hlgroup, iconHl_group, txt_hl_group)
-	return sep_l_hlgroup .. sep_l .. iconHl_group .. icon .. " " .. txt_hl_group .. " " .. txt .. sep_r
-end
-
 M.ui = {
 	theme = "chadracula",
 	-- hl_override = {
@@ -25,26 +15,8 @@ M.ui = {
 		--  more opts
 		order = { "treeOffset", "buffers", "tabs", "btns" },
 	},
-	-- statusline = {
-	-- 	theme = "default",
-	-- 	separator_style = custom_sep_style,
-	-- 	order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
-	-- 	modules = {
-	-- 		cursor = function()
-	-- 			return "%#St_pos_sep#" .. custom_sep_style.left .. "%#St_pos_icon# %#St_Pos_text# L:%l | C:%c "
-	-- 		end,
-	-- 	},
-	-- },
-	statusline = {
-		theme = "minimal",
-		separator_style = "round",
-		order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "cwd" },
-		modules = {
-			cursor = function()
-				return gen_block("", "L:%l | C:%c", "%#St_Pos_sep#", "%#St_Pos_bg#", "%#St_Pos_txt#")
-			end,
-		},
-	},
+
+	statusline = require("configs.stlineconf"),
 	nvdash = {
 		load_on_startup = true,
 		buttons = {
---
+++ lua/configs/stlineconf.lua
@@ -0,0 +1,36 @@
+-- Use this for default style
+-- local custom_sep_style = { left = "█", right = "" }
+
+-- Use this for minimal style
+local sep_l = ""
+local sep_r = "%#St_sep_r#" .. "" .. " %#ST_EmptySpace#"
+local function gen_block(icon, txt, sep_l_hlgroup, iconHl_group, txt_hl_group)
+	return sep_l_hlgroup .. sep_l .. iconHl_group .. icon .. " " .. txt_hl_group .. " " .. txt .. sep_r
+end
+
+local override_cursor = function()
+	return gen_block("", "L:%l | C:%c", "%#St_Pos_sep#", "%#St_Pos_bg#", "%#St_Pos_txt#")
+end
+-- Config for default
+-- local st_line = {
+-- 	theme = "default",
+-- 	separator_style = custom_sep_style,
+-- 	order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+-- 	modules = {
+-- 		cursor = function()
+-- 			return "%#St_pos_sep#" .. custom_sep_style.left .. "%#St_pos_icon# %#St_Pos_text# L:%l | C:%c "
+-- 		end,
+-- 	},
+-- }
+
+-- config for minimal
+local st_line = {
+	theme = "minimal",
+	separator_style = "round",
+	order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "cwd" },
+	modules = {
+		cursor = override_cursor,
+	},
+}
+
+return st_line

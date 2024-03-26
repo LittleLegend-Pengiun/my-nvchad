769203eaea7ead1d2d5a0096e16bad34f5d74f97
Hoang Anh <hoanganhleboy@gmail.com>
2024-03-26 07:52:50 +0700

    Custom minimal style of statusline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,6 +1,15 @@
 local M = {}
-
 -- local hightlights = require("configs.hightlights")
+
+-- Use this for default style
+-- local custom_sep_style = { left = "█", right = "" }
+
+-- Use this for minimal style
+local sep_l = ""
+local sep_r = "%#St_sep_r#" .. "" .. " %#ST_EmptySpace#"
+local function gen_block(icon, txt, sep_l_hlgroup, iconHl_group, txt_hl_group)
+	return sep_l_hlgroup .. sep_l .. iconHl_group .. icon .. " " .. txt_hl_group .. " " .. txt .. sep_r
+end
 
 M.ui = {
 	theme = "chadracula",
@@ -16,13 +25,23 @@ M.ui = {
 		--  more opts
 		order = { "treeOffset", "buffers", "tabs", "btns" },
 	},
+	-- statusline = {
+	-- 	theme = "default",
+	-- 	separator_style = custom_sep_style,
+	-- 	order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+	-- 	modules = {
+	-- 		cursor = function()
+	-- 			return "%#St_pos_sep#" .. custom_sep_style.left .. "%#St_pos_icon# %#St_Pos_text# L:%l | C:%c "
+	-- 		end,
+	-- 	},
+	-- },
 	statusline = {
-		theme = "default",
-		separator_style = { left = "█", right = "" },
-		order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+		theme = "minimal",
+		separator_style = "round",
+		order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "cwd" },
 		modules = {
 			cursor = function()
-				return "%#St_pos_sep#" .. "█" .. "%#St_pos_icon# %#St_Pos_text# L:%l | C:%c "
+				return gen_block("", "L:%l | C:%c", "%#St_Pos_sep#", "%#St_Pos_bg#", "%#St_Pos_txt#")
 			end,
 		},
 	},

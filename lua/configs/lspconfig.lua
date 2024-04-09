ec2d9a909f387b2db9f5ca3273af8add4a783215
Hoang Anh <hoanganhleboy@gmail.com>
2024-04-09 10:14:03 +0700

    Fix the bug of gopls auto complete

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -45,17 +45,17 @@ lspconfig.gopls.setup({
 	cmd = { "gopls" },
 	filename = { "go", "gomod", "gowork", "gotmpl" },
 	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-	settings = {
-		gopls = {
-			completeUnimported = true,
-			usePlaceholders = true,
-			analyses = {
-				unusedparam = true,
-			},
-			staticcheck = true,
-			gofumpt = true,
-		},
-	},
+	-- settings = {
+	-- 	gopls = {
+	-- 		completeUnimported = true,
+	-- 		usePlaceholders = true,
+	-- 		analyses = {
+	-- 			unusedparam = true,
+	-- 		},
+	-- 		staticcheck = true,
+	-- 		gofumpt = true,
+	-- 	},
+	-- },
 })
 
 -- Config for typescript
